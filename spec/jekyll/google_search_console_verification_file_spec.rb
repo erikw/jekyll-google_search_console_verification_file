74c99e4f15b112a1de62f21085a6b6261f9064b7
Erik Westrup <erik.westrup@gmail.com>
2021-06-28 15:52:11 +0200

    $ gem bundle gem jekyll-google_search_console_verification_file

---
+++ spec/jekyll/google_search_console_verification_file_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Jekyll::GoogleSearchConsoleVerificationFile do
+  it "has a version number" do
+    expect(Jekyll::GoogleSearchConsoleVerificationFile::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
